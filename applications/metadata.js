20763343151bdf1dadea72c3b823c0bce5dd2e00
Kasen IO <kasenvr@gmail.com>
2020-06-30 13:54:28 -0400

    Inventory app initial release (v2.0.2)

--- applications/metadata.js
+++ applications/metadata.js
@@ -1,50 +1,59 @@
 var metadata = { "applications": [
-      {
-         "isActive": true,
-         "directory": "doppelganger-mirror",
-         "name": "Doppelganger-Mirror",
-         "description": "Generate a double of your avatar that only you can see. This allows you to examine your avatar and see how you move. The doppelganger can be grabbed and positioned however you want.",
-         "jsfile": "doppelganger-mirror/app-doppleganger.js",
-         "icon": "doppelganger-mirror/doppleganger-i.svg",
-         "caption": "MIRROR"
-      },
-      {
-         "isActive": true,
-         "directory": "spectator-camera",
-         "name": "Spectator Camera",
-         "description": "Give you a video camera that can display its image on your monitor screen for video capture. It can capture from the camera or from the VR Headset. It can also take classic and spherical 360 snapshots (equirectangular format). Definitely a must.",
-         "jsfile": "spectator-camera/spectatorCamera.js",
-         "icon": "spectator-camera/spectator-i.svg",
-         "caption": "SPECTATOR"
-      },
-      {
-         "isActive": true,
-         "directory": "gravimeter",
-         "name": "Gravimeter",
-         "description": "This application returns a record of the orientation and the position of your avatar. This tool becomes very helpful when you want to build on a sphere with a radial gravity, where it's arduous to evaluate the perpendicularity of the floor.",
-         "jsfile": "gravimeter/gravimeter.js",
-         "icon": "gravimeter/gravimeter_icon.png",
-         "caption": "GRAVITY"
-      },
-      {
-         "isActive": true,
-         "directory": "record",
-         "name": "Record",
-         "description": "With this application, you can make a recording of your avatar (movements and voice), and replay it as a copy of yourself. (Requires an Assignment Client Script to be run on the domain.)",
-         "jsfile": "record/record.js",
-         "icon": "record/icon.svg",
-         "caption": "RECORD"
-      },
-      {
-         "isActive": true,
-         "directory": "domain-navigator",
-         "name": "Domain Navigator",
-         "description": "This tool is to help teleporting yourself rapidly where you need in a domain in a couple of clicks, without having to enter numbers in a path. Ideal for those who are working on large landscapes. (Precision: 80 meters.)",
-         "jsfile": "domain-navigator/dom_nav.js",
-         "icon": "domain-navigator/dom_nav_icon_i.png",
-         "caption": "DOM NAV"
-     },
-     {
+    {
+        "isActive": true,
+        "directory": "inventory-app",
+        "name": "Inventory App",
+        "description": "The inventory app allows you to store, use, and share items in Vircadia.",
+        "jsfile": "inventory-app/inventory.js",
+        "icon": "inventory-app/inventory-i.svg",
+        "caption": "INVENTORY"
+    },
+    {
+        "isActive": true,
+        "directory": "doppelganger-mirror",
+        "name": "Doppelganger-Mirror",
+        "description": "Generate a double of your avatar that only you can see. This allows you to examine your avatar and see how you move. The doppelganger can be grabbed and positioned however you want.",
+        "jsfile": "doppelganger-mirror/app-doppleganger.js",
+        "icon": "doppelganger-mirror/doppleganger-i.svg",
+        "caption": "MIRROR"
+    },
+    {
+        "isActive": true,
+        "directory": "spectator-camera",
+        "name": "Spectator Camera",
+        "description": "Give you a video camera that can display its image on your monitor screen for video capture. It can capture from the camera or from the VR Headset. It can also take classic and spherical 360 snapshots (equirectangular format). Definitely a must.",
+        "jsfile": "spectator-camera/spectatorCamera.js",
+        "icon": "spectator-camera/spectator-i.svg",
+        "caption": "SPECTATOR"
+    },
+    {
+        "isActive": true,
+        "directory": "gravimeter",
+        "name": "Gravimeter",
+        "description": "This application returns a record of the orientation and the position of your avatar. This tool becomes very helpful when you want to build on a sphere with a radial gravity, where it's arduous to evaluate the perpendicularity of the floor.",
+        "jsfile": "gravimeter/gravimeter.js",
+        "icon": "gravimeter/gravimeter_icon.png",
+        "caption": "GRAVITY"
+    },
+    {
+        "isActive": true,
+        "directory": "record",
+        "name": "Record",
+        "description": "With this application, you can make a recording of your avatar (movements and voice), and replay it as a copy of yourself. (Requires an Assignment Client Script to be run on the domain.)",
+        "jsfile": "record/record.js",
+        "icon": "record/icon.svg",
+        "caption": "RECORD"
+    },
+    {
+        "isActive": true,
+        "directory": "domain-navigator",
+        "name": "Domain Navigator",
+        "description": "This tool is to help teleporting yourself rapidly where you need in a domain in a couple of clicks, without having to enter numbers in a path. Ideal for those who are working on large landscapes. (Precision: 80 meters.)",
+        "jsfile": "domain-navigator/dom_nav.js",
+        "icon": "domain-navigator/dom_nav_icon_i.png",
+        "caption": "DOM NAV"
+    },
+    {
         "isActive": true,
         "directory": "vr-grabscale",
         "name": "VR Grabscale",
@@ -52,8 +61,8 @@ var metadata = { "applications": [
         "jsfile": "vr-grabscale/VRBuildGrabScale.js",
         "icon": "vr-grabscale/logo.png",
         "caption": "VR SCALE"
-     },
-     {
+    },
+    {
         "isActive": true,
         "directory": "radar",
         "name": "Radar",
@@ -63,13 +72,13 @@ var metadata = { "applications": [
         "caption": "RADAR"
     },
     {
-       "isActive": true,
-       "directory": "refresh-app",
-       "name": "Refresh App",
-       "description": "Add two buttons that refresh your avatar and attachments.",
-       "jsfile": "refresh-app/refreshApp.js",
-       "icon": "refresh-app/refresh-circle-w.png",
-       "caption": "REFRESH"
+        "isActive": true,
+        "directory": "refresh-app",
+        "name": "Refresh App",
+        "description": "Add two buttons that refresh your avatar and attachments.",
+        "jsfile": "refresh-app/refreshApp.js",
+        "icon": "refresh-app/refresh-circle-w.png",
+        "caption": "REFRESH"
     },
     {
         "isActive": true,
