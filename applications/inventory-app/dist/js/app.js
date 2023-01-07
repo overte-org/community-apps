3b1d24bfb4451c1fc2a2249d2092be2dbb4fe230
steve hocktail <mvboy39@gmail.com>
2023-01-06 21:11:13 -0500

    added function to use item

--- applications/inventory-app/dist/js/app.js
+++ applications/inventory-app/dist/js/app.js
@@ -36,26 +36,54 @@
         document.getElementById("app").innerHTML = createFolderDiv("Inventory", inventory);
     }
 
-    function newItem(name, type, url, folderPath) {
+    function getFolder(path) {
         var currentFolder = inventory;
         depthLoop:
-        for (var depth = 0; depth < folderPath.length; depth++) {
+        for (var depth = 0; depth < path.length; depth++) {
             for (var subfolder = 0; subfolder < currentFolder.length; subfolder++) {
-                if (currentFolder[subfolder]["name"] === folderPath[depth] && "items" in currentFolder[subfolder]) {
-                    currentFolder = currentFolder[subfolder]["items"];
-                    continue depthLoop;
+                if (currentFolder[subfolder]["name"] === path[depth]) {
+                    if ("items" in currentFolder[subfolder]) {
+                        currentFolder = currentFolder[subfolder]["items"];
+                        continue depthLoop;
+                    } else {
+                        alert("getFolder: " + path[depth] + " is an item, not a folder");
+                        return [];
+                    }
                 }
             }
-            alert("folder not found!");
-            return;
+            alert("getFolder: folder " + path[depth] + " not found!");
+            return [];
         }
-        for (var folderItem = 0; folderItem < currentFolder.length; i++) {
-            if (currentFolder[folderItem]["name"] === name) {
-                alert("item with this name already exists!");
+        return currentFolder;
+    }
+
+    function getItem(path) {
+        const folder = getFolder(path.slice(0, path.length - 1));
+        if (folder.length === 0) {
+            return {};
+        }
+        const name = path[path.length - 1];
+        for (var folderItem = 0; folderItem < folder.length; folderItem++) {
+            if (folder[folderItem]["name"] === name) {
+                return folder[folderItem];
+            }
+        }
+        alert("getItem: item " + JSON.stringify(path) + " not found!");
+        return {};
+    }
+
+    function newItem(path, type, url) {
+        const name = path[path.length - 1];
+        const folder = path.length > 1 ? getFolder(path.slice(0, path.length - 1)) : inventory;
+        alert(name);
+        alert(JSON.stringify(folder));
+        for (var folderItem = 0; folderItem < folder.length; folderItem++) {
+            if (folder[folderItem]["name"] === name) {
+                alert("newItem: item with name " + name + " already exists!");
                 return;
             }
         }
-        currentFolder.push({name:name,type:type,url:url});
+        folder.push({name:name,type:type,url:url});
         sendEvent("web-to-script-inventory", inventory);
         refreshInventoryView();
     }
@@ -83,12 +111,24 @@
         refreshInventoryView();
     }
 
+    function useItem(path) {
+        const item = getItem(path);
+        if (Object.keys(item).length > 0) {
+            sendEvent("use-item", item);
+        }
+    }
+
     function scriptToWebInventory(data) {
         inventory = data;
         refreshInventoryView();
-        //newItem("cool test item", "UNKNOWN", "https://example.social/yourface.jpg", ["recursiontest", "recursion2", "recursion3"]);
+        //newItem(["recursiontest", "recursion2", "recursion3", "cool test item"], "UNKNOWN", "https://example.social/yourface.jpg");
         //newFolder(["recursiontest", "recursion2", "recursion3.1", "recursion4"]);
         //newFolder(["new folder"]);
+        //newFolder(["woody"]);
+        //newItem(["hello"], "UNKNOWN", "example.hello");
+        //newItem(["recursiontest", "hello again"], "UNKNOWN", "example.again");
+        //newItem(["woody", "avatar"], "AVATAR", "https://cdn-1.vircadia.com/us-e-1/Bazaar/Avatars/Woody/mannequin.fst");
+        //useItem(["woody", "avatar"]);
     }
 
     EventBridge.scriptEventReceived.connect(function(message) {
@@ -104,5 +144,5 @@
         }
     });
 
-    sendEvent("ready", "");
+    sendEvent("ready", {});
 }());
