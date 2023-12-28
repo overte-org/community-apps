8f2d5cfd625ce07d0c6aee5b7c5bd586c1acddba
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2023-12-28 14:07:02 -0500

    Fix initial position of the doppleganger
    Fix initial position of the doppleganger

--- tutorial/doppleganger.js
+++ tutorial/doppleganger.js
@@ -172,7 +172,7 @@ Doppleganger.prototype = {
         }
 
         this.frame = 0;
-        this.position = options.feetPosition || Vec3.sum(avatar.feetPosition, Quat.getForward(avatar.orientation));
+        this.position = options.position || Vec3.sum(avatar.feetPosition, Quat.getForward(avatar.orientation));
         this.orientation = options.orientation || avatar.orientation;
         this.skeletonModelURL = avatar.skeletonModelURL;
         this.jointStateCount = 0;
