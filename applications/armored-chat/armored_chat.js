c4a05f976fdc8674951eaba631f17e97652d8952
Armored Dragon <publicmail@armoreddragon.com>
2024-02-22 16:56:14 -0600

    Cleanup Changed branding from Armored Dragon to Overte. Added to metadata.js.
    Signed-off-by: Armored Dragon <publicmail@armoreddragon.com>

--- applications/armored-chat/armored_chat.js
+++ applications/armored-chat/armored_chat.js
@@ -9,27 +9,22 @@
 
 (function () {
   "use strict";
-  // TODO: Theme consistency
-  // TODO: Dark Theme/Light theme
-  // TODO: Encryption
-  // TODO: Image embedding
+  // TODO: Encryption + PMs
   // TODO: Find window init event method
 
-  var app_is_visible = true;
+  var app_is_visible = false;
   var settings = {
     max_history: 250,
-    // show_typing_indicator: true,
-    // show_speech_bubble: true,
     compact_chat: false,
     external_window: false,
   };
-  var app_data = { app_uuid: Uuid.generate(), current_page: "domain" };
-  // var encryption = {  };
-
+  var app_data = { current_page: "domain" };
   // Global vars
   var ac_tablet;
   var chat_overlay_window;
   var app_button;
+  const channels = ["domain", "local", "system"];
+  var max_local_distance = 20; // Maximum range for the local chat
 
   startup();
 
@@ -38,23 +33,25 @@
 
     app_button = ac_tablet.addButton({
       icon: Script.resolvePath("./img/icon.png"),
-      text: "A-CHAT",
+      text: "CHAT",
+      isActive: app_is_visible,
     });
 
     // When script ends, remove itself from tablet
     Script.scriptEnding.connect(function () {
+      console.log("Shutting Down");
       ac_tablet.removeButton(app_button);
-      console.log("Closing");
       chat_overlay_window.close();
     });
 
-    _openWindow();
-
     // Overlay button toggle
     app_button.clicked.connect(toggleMainChatWindow);
+
+    _openWindow();
   }
   function toggleMainChatWindow() {
     app_is_visible = !app_is_visible;
+    console.log(`App is now ${app_is_visible ? "visible" : "hidden"}`);
     app_button.editProperties({ isActive: app_is_visible });
     chat_overlay_window.visible = app_is_visible;
 
@@ -65,38 +62,35 @@
   }
   function _openWindow() {
     chat_overlay_window = new Desktop.createWindow(Script.resourcesPath() + "qml/hifi/tablet/DynamicWebview.qml", {
-      title: "Armored-Chat",
+      title: "Overte Chat",
       size: { x: 550, y: 400 },
       additionalFlags: Desktop.ALWAYS_ON_TOP,
-      visible: app_is_visible,
+      visible: app_is_visible, // FIXME Invalid?
       presentationMode: Desktop.PresentationMode.VIRTUAL,
     });
+    chat_overlay_window.visible = app_is_visible; // The "visible" field in the Desktop.createWindow does not seem to work. Force set it to false
+
     chat_overlay_window.closed.connect(toggleMainChatWindow);
     chat_overlay_window.sendToQml({ url: Script.resolvePath("./index.html") });
     // FIXME: Loadsettings need to happen after the window is initialized?
-    Script.setTimeout(_loadSettings, 1000);
+    // Script.setTimeout(_loadSettings, 1000);
     chat_overlay_window.webEventReceived.connect(onWebEventReceived);
   }
 
   // Initialize default message subscriptions
   Messages.subscribe("chat");
-  Messages.subscribe("system");
-  // Messages.subscribe() // TODO: Get unique avatar identifier
+  // Messages.subscribe("system");
+
   Messages.messageReceived.connect(receivedMessage);
 
   function receivedMessage(channel, message) {
+    console.log(`Received message:\n${message}`);
     var message = JSON.parse(message);
 
-    if (message.action == "change_setting") return;
-
     channel = channel.toLowerCase();
-
     if (channel !== "chat") return;
 
     message.channel = message.channel.toLowerCase();
-
-    console.log(channel);
-    console.log(JSON.stringify(message));
 
     // For now, while we are working on superseding Floof, we will allow compatibility with it.
     // If for_app exists, it came from us and we are just sending the message so Floof can read it.
@@ -104,22 +98,23 @@
     if (message.for_app) return;
 
     // Check the channel is valid
-    if (message.channel !== "domain" && message.channel !== "local" && message.channel !== "system") return;
+    if (!channels.includes(message.channel)) return;
 
+    // FIXME: Not doing distance check?
     // If message is local, and if player is too far away from location, don't do anything
-    if (channel === "local" && !Vec3.withinEpsilon(MyAvatar.position, message.position, 20)) return;
+    if (channel === "local" && Vec3.distance(MyAvatar.position, message.position) < max_local_distance) return;
 
-    if (message.type === "TransmitChatMessage") message.action = "send_chat_message";
+    // Floof chat compatibility.
+    if (message.type) delete message.type;
 
     // Update web view of to new message
-    // FIXME: this needs to be changed to work with appui
-    chat_overlay_window.emitScriptEvent(JSON.stringify(message));
+    _emitEvent({ type: "show_message", ...message });
 
     // Display on popup chat area
     _overlayMessage({ sender: message.displayName, message: message });
   }
   function onWebEventReceived(event) {
-    console.log(event);
+    console.log(`New web event:\n${event}`);
     // FIXME: Lazy!
     // Checks to see if the event is a JSON object
     if (!event.includes("{")) return;
@@ -127,36 +122,43 @@
     var parsed = JSON.parse(event);
 
     // Not our app? Not our problem!
-    if (parsed.app !== "ArmoredChat") return;
+    // if (parsed.app !== "ArmoredChat") return;
 
-    if (parsed.action === "change_page") {
-      app_data.current_page = parsed.message;
-      return;
+    switch (parsed.type) {
+      case "page_update":
+        app_data.current_page = parsed.page;
+        break;
+
+      case "send_message":
+        _sendMessage(parsed.message);
+        break;
+
+      case "open_url":
+        Window.openUrl(parsed.message.toString());
+        break;
+
+      case "setting_update":
+        // Update local settings
+        settings[parsed.setting_name] = parsed.setting_value;
+        // Save local settings
+        _saveSettings();
+
+        switch (parsed.setting_name) {
+          case "external_window":
+            console.log(parsed.setting_value);
+            chat_overlay_window.presentationMode = parsed.setting_value ? Desktop.PresentationMode.NATIVE : Desktop.PresentationMode.VIRTUAL;
+            break;
+        }
+        break;
+
+      case "initialized":
+        _loadSettings();
+        break;
     }
-
-    if (parsed.action === "change_setting") {
-      settings[parsed.message.setting] = parsed.message.value;
-
-      console.log(JSON.stringify(parsed.message));
-
-      _saveSettings();
-      switch (parsed.message.setting) {
-        case "external_window":
-          chat_overlay_window.presentationMode = parsed.message.value ? Desktop.PresentationMode.NATIVE : Desktop.PresentationMode.VIRTUAL;
-          break;
-      }
-
-      if (parsed.message.setting === "vr_safe_mode") {
-        chat_overlay_window.close();
-        // close window
-        // change settings
-      }
-    }
-    if (parsed.action === "send_chat_message") return _sendMessage(parsed.message);
-    if (parsed.action === "open_url") Window.openUrl(parsed.message.toString());
-
-    // Send to specific user (DM)
   }
+  //
+  // Sending messages
+  // These functions just shout out their messages. We are listening to messages in an other function, and will record all heard messages there
   function _sendMessage(message) {
     Messages.sendMessage(
       "chat",
@@ -185,9 +187,8 @@
     // Show overlay of the message you sent
     _overlayMessage({ sender: MyAvatar.sessionDisplayName, message: message });
   }
-  // TODO: Create new overlay system
   function _overlayMessage(message) {
-    // Foofchat compatibility
+    // Floofchat compatibility
     // This makes it so that our own messages are not rendered.
     // For now, Floofchat has priority over notifications as they use a strange system I don't want to touch yet.
     if (!message.action) return;
@@ -201,15 +202,6 @@
       })
     );
   }
-  function _sendUpdateMessage(message_packet = { setting_name, setting_value }) {
-    chat_overlay_window.emitScriptEvent(
-      JSON.stringify({
-        setting_name: message_packet.setting_name,
-        setting_value: message_packet.setting_value,
-        action: "change_setting",
-      })
-    );
-  }
   function _loadSettings() {
     console.log("Loading config");
     settings = Settings.getValue("ArmoredChat-Config", settings);
@@ -218,23 +210,25 @@
 
     // Compact chat
     if (settings.compact_chat) {
-      _sendUpdateMessage({
-        setting_name: "compact-chat",
-        setting_value: true,
-      });
+      _emitEvent({ type: "setting_update", setting_name: "compact_chat", setting_value: true });
     }
 
     // External Window
     if (settings.external_window) {
       chat_overlay_window.presentationMode = settings.external_window ? Desktop.PresentationMode.NATIVE : Desktop.PresentationMode.VIRTUAL;
-      _sendUpdateMessage({
-        setting_name: "external_window",
-        setting_value: true,
-      });
+      _emitEvent({ type: "setting_update", setting_name: "external_window", setting_value: true });
     }
   }
   function _saveSettings() {
     console.log("Saving config");
     Settings.setValue("ArmoredChat-Config", settings);
+  }
+  /**
+   * Emit a packet to the HTML front end. Easy communication!
+   * @param {Object} packet - The Object packet to emit to the HTML
+   * @param {("setting_update"|"show_message")} packet.type - The type of packet it is
+   */
+  function _emitEvent(packet = { type: "" }) {
+    chat_overlay_window.emitScriptEvent(JSON.stringify(packet));
   }
 })();
