21180144a6666202b3542ca4461d2c87d380f5c6
Armored Dragon <publicmail@armoreddragon.com>
2024-02-25 14:14:08 -0600

    Message saving Now saves messages and reloads existing messages even when the window is not open.
    Signed-off-by: Armored Dragon <publicmail@armoreddragon.com>

--- applications/armored-chat/armored_chat.js
+++ applications/armored-chat/armored_chat.js
@@ -10,7 +10,6 @@
 (function () {
   "use strict";
   // TODO: Encryption + PMs
-  // TODO: Find window init event method
 
   var app_is_visible = false;
   var settings = {
@@ -25,6 +24,7 @@
   var app_button;
   const channels = ["domain", "local", "system"];
   var max_local_distance = 20; // Maximum range for the local chat
+  var message_history = Settings.getValue("ArmoredChat-Messages", []);
 
   startup();
 
@@ -72,8 +72,6 @@
 
     chat_overlay_window.closed.connect(toggleMainChatWindow);
     chat_overlay_window.sendToQml({ url: Script.resolvePath("./index.html") });
-    // FIXME: Loadsettings need to happen after the window is initialized?
-    // Script.setTimeout(_loadSettings, 1000);
     chat_overlay_window.webEventReceived.connect(onWebEventReceived);
   }
 
@@ -104,11 +102,18 @@
     // If message is local, and if player is too far away from location, don't do anything
     if (channel === "local" && Vec3.distance(MyAvatar.position, message.position) < max_local_distance) return;
 
-    // Floof chat compatibility.
-    if (message.type) delete message.type;
+    // NOTE: Floof chat compatibility.
+    message.type = "show_message";
 
     // Update web view of to new message
     _emitEvent({ type: "show_message", ...message });
+
+    // Save message to our history
+    let saved_message = message;
+    delete saved_message.position;
+    message_history.push(message);
+    if (message_history.length > settings.max_history) message_history.shift();
+    Settings.setValue("ArmoredChat-Messages", message_history);
 
     // Display on popup chat area
     _overlayMessage({ sender: message.displayName, message: message });
@@ -120,9 +125,6 @@
     if (!event.includes("{")) return;
 
     var parsed = JSON.parse(event);
-
-    // Not our app? Not our problem!
-    // if (parsed.app !== "ArmoredChat") return;
 
     switch (parsed.type) {
       case "page_update":
@@ -218,6 +220,13 @@
       chat_overlay_window.presentationMode = settings.external_window ? Desktop.PresentationMode.NATIVE : Desktop.PresentationMode.VIRTUAL;
       _emitEvent({ type: "setting_update", setting_name: "external_window", setting_value: true });
     }
+
+    // Refill the history with the saved messages
+    message_history.forEach((message) => {
+      delete message.action;
+      console.log(`Prefilling ${JSON.stringify(message)}`);
+      _emitEvent({ type: "show_message", ...message });
+    });
   }
   function _saveSettings() {
     console.log("Saving config");
